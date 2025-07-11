5899e70ae4ed7bfbef347e353322d48e10835b5e
NabilSo <nabilsounaine40@gmail.com>
2025-07-11 16:13:59 +0100

    two sum fix

--- optional-tasks/OpT1-sum-of-two/sum-of-two.py
+++ optional-tasks/OpT1-sum-of-two/sum-of-two.py
@@ -1,11 +1,8 @@
 def sumoftwo(nums, target):
-    result = []
-    for i in range(1,len(nums)):
-        if (  nums[i] + nums[i - 1] == target):
-            result.append(i - 1)
-            result.append(i)  
-            break
-    return result
-
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
 test = [5,6,9,8,7,1,3,5,6,4]
 print(sumoftwo(test, 8))
